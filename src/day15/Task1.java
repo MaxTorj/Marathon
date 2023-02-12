1942f31515f1fe364ec359bc328ab67fedb05ea8
Maksim Torzhkov <maxtorj90@gmail.com>
2023-02-12 19:14:37 +0300

    15 day added

--- src/day15/Task1.java
+++ src/day15/Task1.java
@@ -7,23 +7,25 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        File file = new File("missing_shoes.txt");
         File shoeFile = new File("shoes.csv");
-
+        Scanner scanner = null;
         try {
-            Scanner scanner = new Scanner(shoeFile);
+            scanner = new Scanner(shoeFile);
+
+            File file = new File("missing_shoes.txt");
             PrintWriter pw = new PrintWriter(file);
 
             while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                String[] warehouse = line.split(";");
+            String[] warehouse = scanner.nextLine().split(";");
 
-                if (Integer.parseInt(warehouse[2]) == 0) {
-                    pw.println(line);
-                }
-
-                pw.close();
+            if (Integer.parseInt(warehouse[2]) == 0) {
+                pw.println(warehouse[0] + ", " + warehouse[1] + ", " + warehouse[2]);
             }
+        }
+
+        pw.close();
+        scanner.close();
+
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
         }
